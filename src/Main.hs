8d132e5162c2cd498c67c46b8af6af9423a17c1e
Steven Fontanella <steven.fontanella@gmail.com>
2021-10-18 23:43:39 -0700

    Switch to haskell-src-exts; add a couple things for parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,44 @@
+import Debug.Trace
+
+import Language.Haskell.Exts.Parser
+import Language.Haskell.Exts.Syntax
+import Language.Haskell.Exts.SrcLoc
+
+-- runParser :: DynFlags -> String -> P a -> ParseResult a
+-- runParser flags str parser = unP parser parseState
+--   where
+--     filename = "<interactive>"
+--     location = mkRealSrcLoc (mkFastString filename) 1 1
+--     buffer = stringToStringBuffer str
+--     parseState = mkPState flags buffer location
+
+data Def = Def
+  deriving (Show)
+data Parsed = Program [Def]
+  deriving (Show)
+
+type Failure = String
+
+declToDef :: Decl SrcSpanInfo -> Def
+-- declToDef decl = trace "\n" $ traceShow decl Def
+declToDef decl = trace "\n" $ traceShow decl $ case decl of
+  PatBind{}  -> Def
+  TypeSig{}  -> Def
+  FunBind{}  -> Def
+  TypeDecl{} -> Def
+  DataDecl{} -> Def
+
+parseFromString :: String -> Either Failure Parsed
+parseFromString str = case parseModule str of
+  ParseOk (Module _ mbHead pragma imports decls) -> Right $ Program $ fmap declToDef decls
+  ParseOk _         -> Left $ "parseFromString: Unexpected parse result"
+  ParseFailed loc s -> Left $ "parseFromString: " <> s <> " at " <> show loc
+
+parseFromFile :: String -> IO Parsed
+parseFromFile = fmap (either error id) . fmap parseFromString . readFile
 
 main = do
-  fName <- readLn
-  contents <- readFile fName
-  putStrLn contents
+  fname <- getLine
+  -- putStrLn contents
+  print =<< parseFromFile fname
+  -- putStrLn contents
