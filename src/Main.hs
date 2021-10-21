4e2f01de11f52c2a6a91789a63b4865e9a817369
Steven Fontanella <steven.fontanella@gmail.com>
2021-10-20 20:56:03 -0700

    Add latest

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 import Debug.Trace
+import Data.String
 
 import Language.Haskell.Exts.Parser
 import Language.Haskell.Exts.Syntax
@@ -12,15 +13,28 @@ import Language.Haskell.Exts.SrcLoc
 --     buffer = stringToStringBuffer str
 --     parseState = mkPState flags buffer location
 
-data Def = Def
+data Def info = Def
   deriving (Show)
-data Parsed = Program [Def]
+data Parsed info = Program [Def info]
   deriving (Show)
+
+type Name = String
+data Table = Table Main.Name [SrcSpan]
+
+-- data Loc = Loc
+--   { locLine :: !Int
+--   , locCol  :: !Int
+--   } deriving (Show, Eq)
+
+-- data Range = Range
+--   { rngFile  :: !String
+--   , rngStart :: !Loc
+--   , rngEnd   :: !Loc
+--   } deriving (Show, Eq)
 
 type Failure = String
 
-declToDef :: Decl SrcSpanInfo -> Def
--- declToDef decl = trace "\n" $ traceShow decl Def
+declToDef :: Decl SrcSpanInfo -> Def info
 declToDef decl = trace "\n" $ traceShow decl $ case decl of
   PatBind{}  -> Def
   TypeSig{}  -> Def
@@ -28,13 +42,13 @@ declToDef decl = trace "\n" $ traceShow decl $ case decl of
   TypeDecl{} -> Def
   DataDecl{} -> Def
 
-parseFromString :: String -> Either Failure Parsed
+parseFromString :: String -> Either Failure (Parsed info)
 parseFromString str = case parseModule str of
   ParseOk (Module _ mbHead pragma imports decls) -> Right $ Program $ fmap declToDef decls
-  ParseOk _         -> Left $ "parseFromString: Unexpected parse result"
+  ParseOk _         -> Left "parseFromString: Unexpected parse result"
   ParseFailed loc s -> Left $ "parseFromString: " <> s <> " at " <> show loc
 
-parseFromFile :: String -> IO Parsed
+parseFromFile :: String -> IO (Parsed info)
 parseFromFile = fmap (either error id) . fmap parseFromString . readFile
 
 main = do
