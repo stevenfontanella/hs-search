1a5fbef47197c2eadc5933b70943bc73f2f9621a
stevenfontanella <steven.fontanella@gmail.com>
2021-10-22 22:18:13 -0700

    start adding argument parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,29 @@
-import Debug.Trace
+import Data.Maybe
 
 import Control.Parse
 import qualified Data.Table as T
 
+import Options.Applicative
+
+data Options = Options
+ { symbol :: String
+ , path   :: FilePath
+ } deriving (Show, Eq)
+
+opts :: Parser Options
+opts = Options
+  <$> argument str (metavar "SYMBOL" <> help "symbol to search for")
+  <*> (fromMaybe "." <$> optional (argument str (metavar "PATH" <> help "directory/file to search in")))
+
+parser = info (opts <**> helper) (fullDesc <> progDesc "search for a symbol")
+
 main = do
-  mod <- parseFromFile =<< getLine
+  opts <- execParser parser
+  print opts
+  -- mod <- parseFromFile =<< getLine
 
-  let syms = symsFromModule mod
-  let fibLocs = syms T.!? "fib"
-  putStrLn $ unlines $ map show fibLocs
+  -- let syms = symsFromModule mod
+  -- let fibLocs = syms T.!? "fib"
+  -- putStrLn $ unlines $ map show fibLocs
 
-  pure ()
+  -- pure ()
