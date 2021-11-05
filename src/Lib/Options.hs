6db6508ae09d9b92f751f213c3e4700efd86c60c
Steven Fontanella <steven.fontanella@gmail.com>
2021-11-04 22:01:29 -0700

    smartcase flag

--- src/Lib/Options.hs
+++ src/Lib/Options.hs
@@ -10,12 +10,14 @@ import Options.Applicative
 data Options = Options
  { symbol :: String
  , path   :: FilePath
+ , smartCase :: Bool
  } deriving (Show, Eq)
 
 opts :: Parser Options
 opts = Options
   <$> argument str (metavar "SYMBOL" <> help "symbol to search for")
   <*> (fromMaybe "." <$> optional (argument str (metavar "PATH" <> help "directory/file to search in")))
+  <*> switch (long "smartcase" <> help "ignore case in SYMBOL if it contains no upper case")
 
 parser = info (opts <**> helper) (fullDesc <> progDesc "search for a symbol")
 
--- src/Main.hs
+++ src/Main.hs
@@ -3,38 +3,46 @@ import qualified Data.Table as T
 import Util
 import Lib.Format
 import Lib.Options
+import Data.SymTable
+import Data.Function
 
+import Data.Char (toLower, isLower)
 import Data.Either (partitionEithers)
 import System.FilePath
 import System.IO
 
-import Data.SymTable
-
 import Debug.Trace
 
-
-findOne :: String -> FilePath -> Mod -> SymTable
-findOne toFind path = foldMapMod $ \info name ->
-  if name == toFind
+findOne :: (String -> Bool) -> FilePath -> Mod -> SymTable
+findOne found path = foldMapMod $ \info name ->
+  if found name
     then singleton path info
     else mempty
 
 putStderrLn :: String -> IO ()
 putStderrLn = hPutStrLn stderr
 
-results :: String -> [FilePath] -> IO SymTable
-results toFind paths = do
+eqInsensitive :: String -> String -> Bool
+eqInsensitive = (==) `on` map toLower
+
+results :: (String -> Bool) -> [FilePath] -> IO SymTable
+results found paths = do
   ~(errs, paths_mods) <- partitionEithers <$> mapM (\path -> fmap (path,) <$> parseFromFile path) paths
   mapM_ putStderrLn errs
 
   let (paths, mods) = unzip paths_mods
   -- pure $ mconcat $ withStrategy (parList rpar) $ zipWith (findOne toFind) paths mods
-  pure $ mconcat $ zipWith (findOne toFind) paths mods
+  pure $ mconcat $ zipWith (findOne found) paths mods
 
 main = do
   -- installHandler keyboardSignal (Catch $ exitImmediately ExitSuccess) Nothing
-  Options symbol path <- parseOpts
+  Options symbol path smartCase <- parseOpts
   files <- filter ((== ".hs") . takeExtension) <$> getFilesUnderFolderOrFile path
+  let found target
+        | all isLower symbol
+        , smartCase = target `eqInsensitive` symbol
 
-  r <- results symbol files
+        | otherwise = target == symbol
+
+  r <- results found files
   formatLn r
